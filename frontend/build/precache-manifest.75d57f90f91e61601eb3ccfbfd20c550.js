b2c4161b999bd63ee1b86f364a6eb1f771e94014
2859135131 <129054277+2859135131@users.noreply.github.com>
2024-02-13 16:59:28 +0800

    Add files via upload

---
+++ frontend/build/precache-manifest.75d57f90f91e61601eb3ccfbfd20c550.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e385ec005fad11bff4dc89826b322ad",
+    "url": "./index.html"
+  },
+  {
+    "revision": "454bf764a29e56e64086",
+    "url": "./static/css/2.bfbf028b.chunk.css"
+  },
+  {
+    "revision": "454bf764a29e56e64086",
+    "url": "./static/js/2.a48d68a0.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.a48d68a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0bca5edf7bd0a1dcc73",
+    "url": "./static/js/main.65296483.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
